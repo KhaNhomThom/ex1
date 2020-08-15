e7c9e46e0ccfdd77a2a13406c209c49e364de2d7
Kittipop Duangjai <mrkittipopduangjai@gmail.com>
2020-08-15 15:07:08 +0700

    master

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return "หน้า user";
+    }
+
+    public function create()
+    {
+        return "สร้าง user";
+    }
+
+    public function showname($name)
+    {
+        return "สวัสดี $name";
+    }
+
+    public function shownameandage($name, $age)
+    {
+        return "สวัสดี $name อายุ $age ปี";
+    }
+}
